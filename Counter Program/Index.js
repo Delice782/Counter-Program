7c5d233710e6af13fabdec84f08971354e889f2b
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-05-24 21:55:32 +0000

    Update Index.js

--- Counter Program/Index.js
+++ Counter Program/Index.js
@@ -1,4 +1,4 @@
-       
+        
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
